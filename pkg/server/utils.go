7cea093df7401683813ed425e6a6a53c99973050
Jian Zeng <anonymousknight96@gmail.com>
2022-04-17 21:44:20 +0800

    doc: update comments

--- pkg/server/utils.go
+++ pkg/server/utils.go
@@ -13,10 +13,8 @@ func writeWithTimeout(conn net.Conn, buf []byte, timeout time.Duration) (n int, 
 	return
 }
 
+// readLine will read as much as it can until the last read character is a newline character.
 func readLine(conn net.Conn, buf []byte, timeout time.Duration) (n int, err error) {
-	// 这个只是特殊场景下的 readLine
-	// rsync 在握手过程中除了 protocol version 跟 module name 以外并不会传输其他数据，而这些数据又是以 '\n' 分割
-	// 所以可以直接尽力读满传进来的 buffer 直到读到 '\n' 为止
 	max := len(buf)
 	for {
 		if timeout > 0 {
