ee507f006bc01aa0c4a52665df72bd4f71782f2b
taoky <me@taoky.moe>
2026-06-08 00:04:28 +0800

    fix: Return non-zero when s.Run fails

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/ustclug/rsync-proxy/cmd"
 )
 
 func main() {
-	_ = cmd.New().Execute()
+	if err := cmd.New().Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
